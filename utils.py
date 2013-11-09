f6d2387efca689db169882ef66e63895df105eb9
bobtwinkles <srkoser+GitHub@gmail.com>
2013-11-09 15:19:29 -0500

    Worky

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ def compile_training_data():
 
     curr.execute("SELECT word, spam, good FROM training_words")
     for row in curr.fetchall():
-        curr.execute("INSERT (word, p_spam, p_good) INTO classifier_cache VALUES
+        curr.execute("INSERT (word, p_spam, p_good) INTO classifier_cache VALUES \
                 (:word, :spam/:sum, :good/:sum)",
                 {"word" : row[0], "spam":row[1], "good":row[2]})
     conn.commit()
