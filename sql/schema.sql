6bb377d68638df09d6f8c36b9416ed6ffc55a0c4
bobtwinkles <srkoser+GitHub@gmail.com>
2013-11-09 16:00:38 -0500

    add diff table and change things

--- sql/schema.sql
+++ sql/schema.sql
@@ -11,14 +11,26 @@ DROP TABLE IF EXISTS training_words;
 CREATE TABLE training_words (
     wid serial unique,
     word varchar(128),
-    spam integer,
-    good integer
+    add_spam integer,
+    add_good integer,
+    del_spam integer,
+    del_good integer
 );
 
 DROP TABLE IF EXISTS classifier_cache;
 CREATE TABLE classifier_cache (
     wid serial unique,
     word varchar(128),
-    p_spam number,
-    p_good number,
+    p_add_spam number,
+    p_add_good number,
+    p_del_spam number,
+    p_del_good number
+);
+
+DROP TABLE IF EXISTS training_diffs;
+CREATE TABLE training_diffs {
+    did serial unique,
+    added text,
+    deled text,
+    is_good boolean
 );
