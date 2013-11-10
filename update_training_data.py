0eea3e4f874fbee6de2d00581ad987d5d051b762
bobtwinkles <srkoser+GitHub@gmail.com>
2013-11-09 20:23:54 -0500

    Fix update_training_data

--- update_training_data.py
+++ update_training_data.py
@@ -12,6 +12,7 @@ def compile_training_data():
     curr.execute("SELECT added, deled, is_good FROM training_diffs")
     #format {"word": [a_spam, d_spam, a_good, d_good]}
     words = {}
+    print "Begin commit begin"
     for row in curr.fetchall():
         added = row[0]
         deled = row[1]
@@ -29,14 +30,20 @@ def compile_training_data():
                 words[word][1] += 1
             else:
                 words[word][3] += 1
+    print "Begin commit word updates"
+    curr.execute("DELETE FROM training_words")
     for word in words:
         word_data = words[word]
         curr.execute("INSERT INTO training_words (word, add_spam, add_good, del_spam, del_good) \
-                VALUES (%(word)s, %(aspam)s, %(agood)s, %(dspam)s, %(dgood)s)) WHERE word = %(word)s \
-                ON DUPLICATE KEY UPDATE add_spam=%(aspam)s, del_spam=%(dspam)s, add_good=%(agood)s, del_good=%(dgood)s",
+                    VALUES (%(word)s, %(aspam)s, %(agood)s, %(dspam)s, %(dgood)s)", 
                 {"word":word, "aspam": word_data[0], "dspam":word_data[1], "agood":word_data[2], "dgood":word_data[3]})
+        #curr.execute("INSERT INTO training_words (word, add_spam, add_good, del_spam, del_good) \
+        #        VALUES (%(word)s, %(aspam)s, %(agood)s, %(dspam)s, %(dgood)s) WHERE word = %(word)s \
+        #        ON DUPLICATE KEY UPDATE add_spam=%(aspam)s, del_spam=%(dspam)s, add_good=%(agood)s, del_good=%(dgood)s",
+        #        {"word":word, "aspam": word_data[0], "dspam":word_data[1], "agood":word_data[2], "dgood":word_data[3]})
         conn.commit()
 
+    print "Begin commiting probabilities"
     curr.execute("SELECT sum(add_spam) + sum(add_good) + sum(del_spam) + sum(del_good) FROM training_words LIMIT 1")
 
     zum = curr.fetchone()
@@ -44,10 +51,11 @@ def compile_training_data():
 
     curr.execute("SELECT word, add_spam, add_good, del_spam, del_good FROM training_words")
     for row in curr.fetchall():
-        curr.execute("INSERT (word, p_add_spam, p_add_good, p_del_spam, p_del_good) INTO classifier_cache VALUES \
-                ($(word)s, $(aspam)s/$(sum)s, $(agood)s/%(sum)s, $(dspam)s/$(sum)s, $(dgood)s/$(sum)s)",
-                {"word" : row[0], "aspam":row[1], "agood":row[2], "dspam":row[3], "dgood":row[4]})
+        curr.execute("INSERT INTO classifier_cache (word, p_add_spam, p_add_good, p_del_spam, p_del_good) VALUES \
+                (%(word)s, %(aspam)s/%(sum)s, %(agood)s/%(sum)s, %(dspam)s/%(sum)s, %(dgood)s/%(sum)s)",
+                {"word" : row[0], "aspam":row[1], "agood":row[2], "dspam":row[3], "dgood":row[4], "sum":zum})
     conn.commit()
+    print "Done"
 
 def get_diff_for_diffid(diffid):
     return "This is a test diff"
