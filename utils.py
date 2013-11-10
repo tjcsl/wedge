e32e76d646d7a101118ceb26ec0d0ddf0fe4bbb9
bobtwinkles <srkoser+GitHub@gmail.com>
2013-11-09 22:34:40 -0500

    Fix bug in classifier

--- utils.py
+++ utils.py
@@ -49,6 +49,10 @@ def get_diff_for_diffid(diffid):
     return addedstring, deledstring
 
 
+def is_blacklisted(word):
+    return not re.match("[\w]+", word) and word in ['is', 'in', 'the', 'for', 'was', 'and', 'of', 'to', 'a', 'he', 'it', 'if']
+
+
 def clean_word(word):
     for i in string.punctuation:
         word = word.replace(i, "")
@@ -64,11 +68,15 @@ def classify(added, deled):
     pspam = 0
     pgood = 0
     for w in added_words:
+        if is_blacklisted(w):
+            continue
         curr.execute("SELECT p_add_spam, p_add_good WHERE word = %(word)s", {"word":w})
         row = curr.fetchone()
         pspam += curr[0]
         pgood += curr[1]
     for w in added_words:
+        if is_blacklisted(w):
+            continue
         curr.execute("SELECT p_del_spam, p_del_good WHERE word = %(word)s", {"word":w})
         row = curr.fetchone()
         pspam += curr[0]
