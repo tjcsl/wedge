0b9abc6c8cc0d604fe9881adcea5a1182513f4cf
Samuel Damashek <samuel.damashek@gmail.com>
2013-11-09 20:27:02 -0500

    Fix sdamafail

--- web/views/training.py
+++ web/views/training.py
@@ -7,7 +7,7 @@ import re
 import lxml.html
 cur = conn.cursor()
 
-def train(revid=None):
+def train(revid2=None):
     if request.method=="POST":
         form = request.form
         constructive = form["constructive"]
@@ -38,9 +38,9 @@ def train(revid=None):
         cur.execute("INSERT INTO training_diffs (added, deled, is_good) VALUES (%s, %s, %s)", (addedstring, deledstring, constructive))
         conn.commit()
 
-    if not revid:
-        revid = randint(57000000, 58500000)
-    c = urlopen("http://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvdiffto=prev&revids=%s" % revid).read()
+    if not revid2:
+        revid2 = randint(57000000, 58500000)
+    c = urlopen("http://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvdiffto=prev&revids=%s" % revid2).read()
     c = json.loads(c)
     if "pages" not in c["query"]:
         return "omg pls exist<br/><a href=\"/train/\">pls refresh</a>"
@@ -48,4 +48,4 @@ def train(revid=None):
     diff = pages.values()[0]["revisions"][0]["diff"]["*"]
     comment = pages.values()[0]["revisions"][0]["comment"]
     print comment
-    return render_template("train.html", diff=diff, diffid=revid, editsummary=comment)
+    return render_template("train.html", diff=diff, diffid=revid2, editsummary=comment)
