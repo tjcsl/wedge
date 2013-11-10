b7de6f5cf631ce516869404f69215d0306028e7e
Samuel Damashek <samuel.damashek@gmail.com>
2013-11-09 21:40:56 -0500

    Make training use utils function

--- web/views/training.py
+++ web/views/training.py
@@ -5,6 +5,7 @@ import json
 from db import conn
 import re
 import lxml.html
+from utils import get_diff_for_diffid
 cur = conn.cursor()
 
 def train(revid2=None):
@@ -12,29 +13,7 @@ def train(revid2=None):
         form = request.form
         constructive = form["constructive"]
         revid = form["diffid"]
-        rev = urlopen("http://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvdiffto=prev&revids=%s" % revid).read()
-        rev = json.loads(rev)
-        pages = rev["query"]["pages"]
-        diff = pages.values()[0]["revisions"][0]["diff"]["*"]
-        diff = lxml.html.document_fromstring(diff)
-        addedlines = diff.xpath("//td[@class='diff-addedline']")
-        deledlines = diff.xpath("//td[@class='diff-deletedline']")
-        addedstring = ""
-        deledstring = ""
-        for i in addedlines:
-            diffchanges = i.xpath("./span[@class='diffchange diffchange-inline']/text()")
-            if not diffchanges:
-                addedstring = addedstring + " " + i.text_content()
-            else:
-                addedstring = addedstring + " " + " ".join(diffchanges)
-        for i in deledlines:
-            diffchanges = i.xpath("./span[@class='diffchange diffchange-inline']/text()")
-            if not diffchanges:
-                deledstring = deledstring + " " + i.text_content()
-            else:
-                deledstring = deledstring + " " + " ".join(diffchanges)
-        print addedstring
-        print deledstring
+        addedstring, delstring = get_diff_for_diffid(revid)
         cur.execute("INSERT INTO training_diffs (added, deled, is_good) VALUES (%s, %s, %s)", (addedstring, deledstring, constructive))
         conn.commit()
 
