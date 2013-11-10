b50aa0b0515bfd54b3b5ab5132c75ca174a748e4
csssuf <csssuf724@gmail.com>
2013-11-09 20:37:45 -0500

    add emailing

--- web/auth.py
+++ web/auth.py
@@ -2,7 +2,7 @@ import hashlib
 from db import conn
 from flask import session, flash, redirect, url_for
 from functools import wraps
-
+from postmark import PMMail
 
 def loginrequired(f):
     @wraps(f)
@@ -36,9 +36,23 @@ def create_account(username, password, email):
     """
     cur = conn.cursor()
     cur.execute("SELECT username FROM users")
-    usernames = [matching[0] for matching in cur.fetchall()]
-    if username in usernames or len(username) < 4:
+    fall = cur.fetchall()
+    usernames = [matching[0] for matching in fall]
+    emails = [matching[2] for matching in fall]
+    if username in usernames or len(username) < 4 or len(email) < 7 or email in emails:
         return False
+    emsg = PMMail(api_key = os.environ.get('POSTMARK_API_KEY'),
+                  subject = "Welcome to wedge!",
+                  sender = "wedge@csssuf.net",
+                  to = email,
+                  text_body = """Hello!
+                  Welcome to wedge, the Wikipedia game. You can get going
+                  immediately; wedge will track all your Wikipedia edits and
+                  automatically classify them. Good luck, and edit well!
+                  
+                  -- The wedge team""",
+                  tag = "welcome")
+    emsg.send()
     passwd_hash = hashlib.sha256(password).hexdigest()
     cur.execute("INSERT INTO users (username, passwd_hash, email) VALUES (%s,%s, %s)",
                 (username, passwd_hash, email))
--- web/views/userinfo.py
+++ web/views/userinfo.py
@@ -1,5 +1,6 @@
 from flask import render_template
+from web.auth import loginrequired
 
-
+@loginrequired
 def uinfo():
     return render_template("userinfo.html", thispage='userinfo')
