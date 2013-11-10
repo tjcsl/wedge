6ee2fc4cfbfe95b33476bbe312ec0b0a77c3a398
fwilson <fwilson@fwilson.me>
2013-11-10 00:37:32 -0500

    userstats :D

--- web/views/userstats.py
+++ web/views/userstats.py
@@ -1,6 +1,14 @@
-from flask import render_template
+from flask import render_template, session
 from web.auth import loginrequired
+from db import conn
+
 
 @loginrequired
 def ustats():
-    return render_template("userstats.html", thispage='userstats')
+    cur = conn.cursor()
+    cur.execute("SELECT page_title, summary, score FROM edits WHERE username=(SELECT wp_username from users where uid=%s)", [str(session["uid"])])
+    edit = cur.fetchall()
+    edits = [{'page': i[0], 'summary': i[1], 'score': i[2]} for i in edit]
+    return render_template("userstats.html",
+                           thispage='userstats',
+                           edits=edits)
