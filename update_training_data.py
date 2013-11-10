826d3830450f0ca1aeee639879c150e8c4499a8a
bobtwinkles <srkoser+GitHub@gmail.com>
2013-11-09 19:26:40 -0500

    Fix shit

--- update_training_data.py
+++ update_training_data.py
@@ -3,17 +3,18 @@ curr = conn.cursor()
 
 def get_word_status(word):
     """ Returns word data in format [a_spam, d_spam, a_good, d_good] """
-    curr.execute("SELECT add_spam, add_good, del_spam, del_good FROM training_words WHERE word = :word", (word,))
+    curr.execute("SELECT add_spam, add_good, del_spam, del_good FROM training_words WHERE word = %(word)s", {"word":word})
     row = curr.fetchone()
+    if row is None: return [0, 0, 0, 0]
     return [row[0], row[1], row[2], row[3]]
 
 def compile_training_data():
-    curr.execute("SELECT added, deled is_good FROM training_diffs")
+    curr.execute("SELECT added, deled, is_good FROM training_diffs")
     #format {"word": [a_spam, d_spam, a_good, d_good]}
     words = {}
     for row in curr.fetchall():
-        added = words[0]
-        deled = words[1]
+        added = row[0]
+        deled = row[1]
         for word in added.split():
             if word not in words:
                 words[word] = get_word_status(word)
@@ -29,11 +30,12 @@ def compile_training_data():
             else:
                 words[word][3] += 1
     for word in words:
+        word_data = words[word]
         curr.execute("INSERT INTO training_words (word, add_spam, add_good, del_spam, del_good) \
-                VALUES (:word, :aspam, :agood, :dspam, :dgood) WHERE word = :word
-                ON DUPLICATE KEY UPDATE add_spam=:aspam, del_spam=:dspam, add_good=:agood, del_good=:dgood",
-                {"word":word, "aspam": words[0], "dspam":words[1], "agood":words[2], "dgood":words[3]})
-
+                VALUES (%(word)s, %(aspam)s, %(agood)s, %(dspam)s, %(dgood)s)) WHERE word = %(word)s \
+                ON DUPLICATE KEY UPDATE add_spam=%(aspam)s, del_spam=%(dspam)s, add_good=%(agood)s, del_good=%(dgood)s",
+                {"word":word, "aspam": word_data[0], "dspam":word_data[1], "agood":word_data[2], "dgood":word_data[3]})
+        conn.commit()
 
     curr.execute("SELECT sum(add_spam) + sum(add_good) + sum(del_spam) + sum(del_good) FROM training_words LIMIT 1")
 
@@ -43,7 +45,7 @@ def compile_training_data():
     curr.execute("SELECT word, add_spam, add_good, del_spam, del_good FROM training_words")
     for row in curr.fetchall():
         curr.execute("INSERT (word, p_add_spam, p_add_good, p_del_spam, p_del_good) INTO classifier_cache VALUES \
-                (:word, :aspam/:sum, :agood/:sum, :dspam/:sum, :dgood/:sum)",
+                ($(word)s, $(aspam)s/$(sum)s, $(agood)s/%(sum)s, $(dspam)s/$(sum)s, $(dgood)s/$(sum)s)",
                 {"word" : row[0], "aspam":row[1], "agood":row[2], "dspam":row[3], "dgood":row[4]})
     conn.commit()
 
