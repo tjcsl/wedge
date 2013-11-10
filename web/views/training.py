44fbf225f3fca3b2696e1a4a962ab9cf2ed170d7
bobtwinkles <srkoser+GitHub@gmail.com>
2013-11-10 02:56:03 -0500

    Prettify training interface and add it to navbar

--- web/views/training.py
+++ web/views/training.py
@@ -2,11 +2,13 @@ from flask import request, render_template
 from random import randint
 from urllib2 import urlopen
 from db import conn
+from web.auth import loginrequired
 import re
 import json
 from utils import get_diff_for_diffid
 cur = conn.cursor()
 
+@loginrequired
 def train(revid2=None):
     if request.method=="POST":
         form = request.form
@@ -16,14 +18,15 @@ def train(revid2=None):
         cur.execute("INSERT INTO training_diffs (added, deled, is_good) VALUES (%s, %s, %s)", (addedstring, deledstring, constructive))
         conn.commit()
 
-    if not revid2:
+    while not revid2:
         revid2 = randint(45000000, 58500000)
-    c = urlopen("http://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvdiffto=prev&revids=%s" % revid2).read()
-    c = json.loads(c)
-    if "pages" not in c["query"]:
-        return "omg pls exist<br/><a href=\"/train/\">pls refresh</a>"
-    pages = c["query"]["pages"]
-    diff = pages.values()[0]["revisions"][0]["diff"]["*"]
-    comment = pages.values()[0]["revisions"][0]["comment"]
-    print comment
-    return render_template("train.html", diff=diff, diffid=revid2, editsummary=comment)
+        c = urlopen("http://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvdiffto=prev&revids=%s" % revid2).read()
+        c = json.loads(c)
+        if "pages" not in c["query"]:
+            revid2 = None
+            continue
+        pages = c["query"]["pages"]
+        diff = pages.values()[0]["revisions"][0]["diff"]["*"]
+        comment = pages.values()[0]["revisions"][0]["comment"]
+        print comment
+        return render_template("train.html", diff=diff, diffid=revid2, editsummary=comment)
